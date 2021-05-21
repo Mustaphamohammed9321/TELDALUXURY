9ff5d749c22f639879a0756e17815cede17f0ff7
Mustapha Mohammed <mustapha.mohammed@skillslab.sidmach.com>
2021-05-21 16:25:16 +0100

    Add project files.

---
+++ THELDALUXURYSERVICES/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace THELDALUXURYECOMMERCE.Controllers
+{
+    public class AdminController : Controller
+    {
+        //Admin Index Page
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
