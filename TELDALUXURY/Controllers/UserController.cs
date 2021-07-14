f44f626d734fcf8f47a721c32fe04ea0a413503e
Mustapha Garba Mohammed <mgmohammed@sidmach.com>
2021-07-14 09:17:12 +0100

    Readjusted the models and started creating views

--- TELDALUXURY/Controllers/UserController.cs
+++ TELDALUXURY/Controllers/UserController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
+using TELDALUXURY.Models;
 
 namespace TELDALUXURY.Controllers
 {
@@ -11,9 +13,34 @@ namespace TELDALUXURY.Controllers
        public ActionResult CreateUser()
         {
             return View();
+       }
+
+
+        [HttpPost]
+        public ActionResult create(mvcUsers userlogin)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:60061/api/Users/CreateUserAccount");
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<CreateUserLogin>("userlogin", userlogin);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(userlogin);
         }
 
-        
+
         // POST: Users
         public ActionResult Login()
         {
